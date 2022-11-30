3b40df29486892d8474b1822eeb8b7c29af4c5b5
temeddix <temeddix@gmail.com>
2022-11-30 18:37:58 +0900

    Add image repetition file

---
+++ make_repeating_image.py
@@ -0,0 +1,31 @@
+import math
+import os
+import numpy as np
+import cv2
+
+IMAGE_WIDTH = 1920
+IMAGE_HEIGHT = 1080
+
+chunk = np.array(
+    [
+        [
+            [255, 255, 255],
+            [255, 0, 0],
+            [0, 0, 255],
+            [255, 255, 255],
+            [0, 0, 0],
+            [0, 255, 255],
+            [255, 255, 0],
+            [0, 0, 0],
+        ]
+    ]
+)
+
+vertical_repeat = math.ceil(IMAGE_HEIGHT / chunk.shape[0])
+horizontal_repeat = math.ceil(IMAGE_WIDTH / chunk.shape[1])
+big_repeated_image = np.tile(chunk, reps=(vertical_repeat, horizontal_repeat, 1))
+color_converted_image = cv2.cvtColor(np.float32(big_repeated_image), cv2.COLOR_RGB2BGR)
+cut_image = color_converted_image[:IMAGE_HEIGHT, :IMAGE_WIDTH]
+
+os.makedirs("./basket", exist_ok=True)
+cv2.imwrite("./basket/repeating_image.png", cut_image)
