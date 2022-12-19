c85fff5fc62580df87eba277d917dde4f767a115
temeddix <temeddix@gmail.com>
2022-12-20 01:15:44 +0900

    Make the code work again

--- make_repeating_image.py
+++ make_repeating_image.py
@@ -18,14 +18,14 @@ chunk = np.array(
             [255, 255, 0],
             [0, 0, 0],
         ]
-    ]
+    ],
+    dtype=np.float32,
 )
 
 vertical_repeat = math.ceil(IMAGE_HEIGHT / chunk.shape[0])
 horizontal_repeat = math.ceil(IMAGE_WIDTH / chunk.shape[1])
 big_repeated_image = np.tile(chunk, reps=(vertical_repeat, horizontal_repeat, 1))  # type: ignore
-big_repeated_matrix = cv2.Mat(big_repeated_image)
-color_converted_image = cv2.cvtColor(big_repeated_matrix, cv2.COLOR_RGB2BGR)
+color_converted_image = cv2.cvtColor(big_repeated_image, cv2.COLOR_RGB2BGR)
 cut_image = color_converted_image[:IMAGE_HEIGHT, :IMAGE_WIDTH]
 
 os.makedirs("./basket", exist_ok=True)
