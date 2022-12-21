aea75fa100e9b0ae3f1d5e1b7d12739350071e11
temeddix <temeddix@gmail.com>
2022-12-22 01:26:04 +0900

    Create explain.py

---
+++ explain.py
@@ -0,0 +1,12 @@
+a = 60
+
+if a > 100:
+    print("it is realllllllly big")
+elif a > 30:
+    print("it is reallllly big")
+elif a > 10:
+    print("it is big")
+elif a > 5:
+    print("it is a bit big")
+else:
+    print("it is not big")
